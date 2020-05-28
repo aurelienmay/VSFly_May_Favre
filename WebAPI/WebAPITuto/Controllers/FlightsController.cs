48c01d83a6b15eb19d19d54e19cd1bde6bdbcf46
aurelienmay <48348880+aurelienmay@users.noreply.github.com>
2020-05-28 22:10:42 +0200

    Get only available flights done.

--- WebAPI/WebAPITuto/Controllers/FlightsController.cs
+++ WebAPI/WebAPITuto/Controllers/FlightsController.cs
@@ -25,7 +25,7 @@ namespace WebAPITuto.Controllers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Flight>>> GetFlights()
         {
-            return await _context.FlightSet.ToListAsync();
+            return await _context.FlightSet.AsQueryable().Where(flight => flight.AvailableSeats != 0).ToListAsync();
         }
 
         // GET: api/Flight/5
